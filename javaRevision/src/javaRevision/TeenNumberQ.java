ded59862f6824cea393d6b21be69107c38a88a33
ciaralw <WAR14101792@belfastmet.ac.uk>
2019-09-26 20:52:46 +0100

    added better way for teen number

--- javaRevision/src/javaRevision/TeenNumberQ.java
+++ javaRevision/src/javaRevision/TeenNumberQ.java
@@ -5,10 +5,10 @@ public class TeenNumberQ {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		TeenNumberQ teenNumberQ = new TeenNumberQ();
-		System.out.println(teenNumberQ.hasTeen(13, 20, 10));
-		System.out.println(teenNumberQ.hasTeen(20, 19, 10));
-		System.out.println(teenNumberQ.hasTeen(20, 10, 13));
-		System.out.println(teenNumberQ.hasTeen(30, 20, 10));
+		System.out.println(teenNumberQ.hasTeen2(13, 20, 10));
+		System.out.println(teenNumberQ.hasTeen2(20, 19, 10));
+		System.out.println(teenNumberQ.hasTeen2(20, 10, 13));
+		System.out.println(teenNumberQ.hasTeen2(30, 20, 10));
 	}
 	
 	public boolean hasTeen(int age1, int age2, int age3) {
@@ -27,5 +27,28 @@ public class TeenNumberQ {
 		}
 		return teen;
 	}
-
+	
+	public boolean hasTeen1(int age1, int age2, int age3) {
+		if(age1 >= 13 && age1 <=19) {
+			return true;
+		}
+		else if(age2 >= 13 && age2 <=19) {
+			return true;
+		}
+		else if(age3 >= 13 && age3 <=19) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	public boolean hasTeen2(int age1, int age2, int age3) {
+		if((age1 >= 13 && age1 <=19) || (age2 >= 13 && age2 <=19) || (age3 >= 13 && age3 <=19)) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
 }
